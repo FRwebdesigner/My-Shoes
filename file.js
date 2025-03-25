96b4cec539a7acd7f276972bb34b95838d1e00bc
ftsoriaromano <ftsoriaromano@gmail.com>
2025-03-25 18:01:59 -0300

    navbar codification with html css js finished

--- file.js
+++ file.js
@@ -2,10 +2,19 @@ let searchform = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
 }
 
 window.onscroll = () =>{
     searchform.classList.remove('active');
+    navbar.classList.remove('active');
 
     if(window.scrollY > 30){
         document.querySelector('header').classList.add('header-active');
