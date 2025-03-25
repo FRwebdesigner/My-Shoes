885b51a5ac9da302587b99c63b81f6e794017e55
ftsoriaromano <ftsoriaromano@gmail.com>
2025-03-25 15:56:07 -0300

    scroll bar creation

--- file.js
+++ file.js
@@ -3,3 +3,14 @@ let searchform = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    searchform.classList.remove('active');
+
+    if(window.scrollY > 30){
+        document.querySelector('header').classList.add('header-active');
+    }
+    else{
+        document.querySelector('header').classList.remove('header-active');
+    }
+}
